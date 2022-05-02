29b580325410416fde64da5a22abf4527ef7b93d
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-05-02 13:56:16 -0400

    Created UserServiceTest class with an empty test method

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
