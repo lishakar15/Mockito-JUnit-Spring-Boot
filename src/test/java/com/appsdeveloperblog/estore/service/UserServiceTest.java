a0443184671c204d5e796d6526ecc7ebaa09eadd
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-05-02 14:53:18 -0400

    Created UserService and UserServiceImpl

--- src/test/java/com/appsdeveloperblog/estore/service/UserServiceTest.java
+++ src/test/java/com/appsdeveloperblog/estore/service/UserServiceTest.java
@@ -1,3 +1,6 @@
+package com.appsdeveloperblog.estore.service;
+
+import com.appsdeveloperblog.estore.service.UserService;
 import org.junit.jupiter.api.Test;
 
 public class UserServiceTest {
@@ -5,6 +8,7 @@ public class UserServiceTest {
     @Test
     void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
         // Arrange
+        UserService userService = new UserServiceImpl();
 
         // Act
 
