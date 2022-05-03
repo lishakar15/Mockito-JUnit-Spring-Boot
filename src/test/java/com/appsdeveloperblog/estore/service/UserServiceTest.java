91f9c8014234045bc6fc5777b4aa3ec369241d6e
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2022-05-03 11:48:52 -0400

    Implemented empty createUser() method

--- src/test/java/com/appsdeveloperblog/estore/service/UserServiceTest.java
+++ src/test/java/com/appsdeveloperblog/estore/service/UserServiceTest.java
@@ -1,7 +1,10 @@
 package com.appsdeveloperblog.estore.service;
 
+import com.appsdeveloperblog.estore.model.User;
 import com.appsdeveloperblog.estore.service.UserService;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UserServiceTest {
 
@@ -9,9 +12,17 @@ public class UserServiceTest {
     void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
         // Arrange
         UserService userService = new UserServiceImpl();
+        String firstName = "Sergey";
+        String lastName  = "Kargopolov";
+        String email = "test@test.com";
+        String password = "12345678";
+        String repeatPassword = "12345678";
 
         // Act
+        User user = userService.createUser(firstName, lastName, email, password, repeatPassword);
 
         // Assert
+        assertNotNull(user, "The createUser() should not have returned null");
+
     }
 }
